ad6af8ec041704d5724d30160f001bd811303404
daniel-ilin <ilinedv@gmail.com>
2022-10-09 15:19:45 -0400

    exposed controller functionality

--- src/stories/Carousel.stories.tsx
+++ src/stories/Carousel.stories.tsx
@@ -1,9 +1,11 @@
 // Showcase images taken from Unsplash - https://unsplash.com/
 // Showcase video taken from Pixabay - https://pixabay.com
 
-import React from "react";
+import React, { useRef } from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import Carousel from "../components/Carousel";
+import { CarouselControllerHandle } from "../components/Carousel/CarouselComponents/Carousel";
+import styles from "./Carousel.stories.module.scss";
 
 export default {
   title: "Carousel",
@@ -19,55 +21,84 @@ let imageUrls = [
 let videoUrl =
   "https://player.vimeo.com/video/577442929?title=0&portrait=0&byline=0&autoplay=1&loop=1&transparent=1";
 
-const Template: ComponentStory<typeof Carousel> = (args) => (
-  <Carousel {...args}>
-    <img
-      src={imageUrls[0]}
-      width="100%"
-      height="100%"
-      style={{ objectFit: "cover", position: "relative" }}
-    ></img>
-    <div
-      style={{
-        backgroundColor: "black",
-        height: "100%",
-        width: "100%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <iframe
-        src={videoUrl}
-        style={{
-          border: "none",
-          objectFit: "fill",
-        }}
-        width="100%"
-        height="100%"
-      ></iframe>
-    </div>
-    <div
-      style={{
-        backgroundColor: "#F9F6EE",
-        height: "100%",
-        width: "100%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        fontSize: "30px",
-      }}
-    >
-      <span>Hello!</span>
-    </div>
-    <img
-      src={imageUrls[1]}
-      width="100%"
-      height="100%"
-      style={{ objectFit: "cover" }}
-    ></img>
-  </Carousel>
-);
+const Template: ComponentStory<typeof Carousel> = (args) => {
+  const controllerRef = useRef<CarouselControllerHandle>(null);
+
+  return (
+    <span>
+      <Carousel {...args} ref={controllerRef}>
+        <img
+          src={imageUrls[0]}
+          width="100%"
+          height="100%"
+          style={{ objectFit: "cover", position: "relative" }}
+        ></img>
+        <div
+          style={{
+            backgroundColor: "black",
+            height: "100%",
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <iframe
+            src={videoUrl}
+            style={{
+              border: "none",
+              objectFit: "fill",
+            }}
+            width="100%"
+            height="100%"
+          ></iframe>
+        </div>
+        <div
+          style={{
+            backgroundColor: "#F9F6EE",
+            height: "100%",
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            fontSize: "30px",
+          }}
+        >
+          <span>Hello!</span>
+        </div>
+        <img
+          src={imageUrls[1]}
+          width="100%"
+          height="100%"
+          style={{ objectFit: "cover" }}
+        ></img>
+      </Carousel>
+      <div className={styles.buttonsContainer}>
+        <button
+          onClick={() => {
+            controllerRef.current && controllerRef.current.shiftLeft();
+          }}
+        >
+          Custom Button: Shift Left
+        </button>
+        <button
+          onClick={() => {
+            controllerRef.current && controllerRef.current.shiftRight();
+          }}
+        >
+          Custom Button: Shift Right
+        </button>
+        <button
+          onClick={() => {
+            controllerRef.current && controllerRef.current.jumpToIndex(2);
+          }}
+        >
+          Custom Button: Jump To Index 2{" "}
+        </button>
+      </div>
+    </span>
+  );
+};
 
 export const FirstStory = Template.bind({});
 
